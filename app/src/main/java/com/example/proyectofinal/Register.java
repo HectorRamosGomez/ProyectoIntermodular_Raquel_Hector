1cdd1da30a6b82226f2109bc1b48614b9f324686
commit <raquelpumaripuita@gmail.com>
2025-11-27 13:02:18 +0000

    login con gogle

--- app/src/main/java/com/example/proyectofinal/Register.java
+++ app/src/main/java/com/example/proyectofinal/Register.java
@@ -1,6 +1,27 @@
 package com.example.proyectofinal;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+
+
 public class Register extends AppCompatActivity {
+
+    TextInputEditText editTextNameandSurname, editTextEmail ,editTextContrasena;
+    Button button1, button2,button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        editTextNameandSurname=findViewById(R.id.NomyApe);
+        editTextEmail= findViewById(R.id.correoelectronico);
+        editTextContrasena= findViewById(R.id.contraseña);
+        button1=findViewById(R.id.boton1);
+        button2=findViewById(R.id.boton2);
+        button3=findViewById(R.id.login_google);
+    }
 }
