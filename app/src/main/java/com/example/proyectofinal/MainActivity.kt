4b1e40a03c59199d5dbfaab661d2701a9fd337ab
hecto <hectorramosgomez717@gmail.com>
2026-02-05 19:06:11 +0100

    primer login

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.proyectofinal
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.android.gms.auth.api.signin.GoogleSingInClient
+import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity(){
 
 
-    private lateinit var auth : FirebaseAuth
-    private lateinit var googleSingInClient : GoogleSingInClient
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState:Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.botonComenzar)
+
+        button.setOnClickListener {
+
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
     }
 }
