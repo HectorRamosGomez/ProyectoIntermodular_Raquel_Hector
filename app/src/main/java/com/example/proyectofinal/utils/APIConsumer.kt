b3dcbedee6da1111e277cdc183373dfe1059fe3a
hecto <hectorramosgomez717@gmail.com>
2026-02-08 18:36:08 +0100

    Cambio ruta APIs

--- app/src/main/java/com/example/proyectofinal/utils/APIConsumer.kt
+++ app/src/main/java/com/example/proyectofinal/utils/APIConsumer.kt
@@ -5,6 +5,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface APIConsumer {
-    @POST("users/check-email")
+    @POST("users/validate_unique_email")
     suspend fun validarEmail(@Body body: Map<String, String>): Response<Map<String, Any>>
 }
